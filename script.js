3dff7124636091a3083493d1a687243cae1a1db3
Kevin Getz <kgetz32@gmail.com>
2022-09-20 10:29:56 -0700

    added checkwin function and call endgame display message

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const xClass = 'x';
 const circleClass = 'o';
 const squareDivs = document.querySelectorAll('.square');
 const board = document.querySelector('game-grid');
+const endgameMessage = document.querySelector('.endgame-message')
+const endGameText = document.querySelector('[endgame-message-text]')
 let circleTurn;
 const winCombinations = [
     [0, 1, 2],
@@ -33,13 +35,34 @@ function clickAction(e) {
     const square =e.target;
     const currentClass = circleTurn ? circleClass : xClass;
     placeMark(square, currentClass)
+    if (checkWin(currentClass)) {
+        //console.log('winner');
+        endGame(false);
+    }
     switchTurns()
-}
+};
 
 function placeMark(square, currentClass){
     square.classList.add(currentClass);
-}
+};
+
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        endGameText.innerText = `${circleTurn ? 'Red' : 'Blue'} Wins!`;
+    }
+    endgameMessage.classList.add('show');
+};
+
+function checkWin(currentClass) {
+    return winCombinations.some(combination => {
+        return combination.every(index => {
+            return squareDivs[index].classList.contains(currentClass)
+        })
+    })
+};
 
 function switchTurns() {
     circleTurn = !circleTurn
-}
+};
